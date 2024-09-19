f489c8fdc2a66f540c35482aa9bf87301c68bfc7
NinipGanteng <hanifurohman05@gmail.com>
2024-09-19 20:46:19 +0700

    y

--- plugins/_blacklist.js
+++ plugins/_blacklist.js
@@ -1,7 +1,7 @@
 export default {
     before: async (m, { conn }) => {
       if (!m.isGroup) return;
-      if (m.sender === "0@s.whatsapp.net") { // Ganti 0 Ke Nomer Target Blacklist, contoh : 6281234567890@s.whatsapp.net
+      if (m.sender === "6282269200291@s.whatsapp.net") { // Ganti 0 Ke Nomer Target Blacklist, contoh : 6281234567890@s.whatsapp.net
         await conn.sendMessage(m.chat, { delete: { ...m.key }});
       }
     }
