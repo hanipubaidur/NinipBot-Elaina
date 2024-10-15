7731f58014e83154c39702d32fc5f41b1e4a3df5
NinipGanteng <hanifurohman05@gmail.com>
2024-10-15 10:25:43 +0700

    hmm

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,29 +1,30 @@
-import { areJidsSameUser } from '@adiwajshing/baileys'
-
-let handler = async (m, { conn, participants, isAdmin }) => {
-    if (!isAdmin) {
-        return m.reply('Perintah ini hanya dapat digunakan oleh admin grup')
+let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
+    if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn)
+      throw false
     }
-
-    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
-    let kickedUser = []
+    let ownerGroup = m.chat.split`-`[0] + "@s.whatsapp.net";
+    if(m.quoted){
+      if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
+      let usr = m.quoted.sender;
+      await conn.groupParticipantsUpdate(m.chat, [usr], "remove"); return;
+    }
+    if (!m.mentionedJid[0]) throw `tag yang mau dikick`;
+    let users = m.mentionedJid.filter(
+      (u) => !(u == ownerGroup || u.includes(conn.user.jid))
+    );
     for (let user of users)
-        if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
-            const res = await conn.groupParticipantsUpdate(m.chat, [user], "remove")
-            kickedUser.concat(res)
-            await delay(1 * 1000)
-        }
-    m.reply(`Mampos Dikick Kau ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
-}
+      if (user.endsWith("@s.whatsapp.net"))
+        await conn.groupParticipantsUpdate(m.chat, [user], "remove");
+  };
+  
+    handler.help = ['kick'].map(v => v + ' @user')
+    handler.tags = ['group']
+    handler.command = /^(kick|kik)$/i
 
-handler.help = ['kick'].map(v => v + ' @user')
-handler.tags = ['group']
-handler.command = /^(kick|kik)$/i
+    handler.owner = false
+    handler.group = true
+    handler.botAdmin = true
+    handler.admin = true // hanya admin grup yang dapat menggunakan perintah ini
 
-handler.owner = false
-handler.group = true
-handler.botAdmin = true
-handler.admin = true // hanya admin grup yang dapat menggunakan perintah ini
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-export default handler
+    export default handler;
