0b23e050a426b535bd87203a52f733281a81eae7
hanipubaidur <hanifurohman05@gmail.com>
2024-12-09 11:30:07 +0700

    Fix Antilink

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,25 +1,27 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+let handler = m => m
 
-export async function before(m, {conn, isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    const isGroupLink = linkRegex.exec(m.text)
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+  let chat = global.db.data.chats[m.chat]
+  let isGroupLink = linkRegex.exec(m.text)
 
-    if (chat.antiLink && isGroupLink && !isAdmin) {
-        if (isBotAdmin) {
-            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-            if (m.text.includes(linkThisGroup)) return !0
-        }
-        await conn.reply(m.chat, `*≡ Tautan terdeteksi*
-            
-Kami tidak mengizinkan link dari grup lain 
-Maaf  ${isBotAdmin ? '' : '\n\nSaya bukan admin jadi saya tidak bisa menghapus pesan'}`, null, { mentions: [m.sender] } )
-        if (isBotAdmin && chat.antiLink) {
-        	await conn.sendMessage(m.chat, { delete: m.key })
-        } else if (!chat.antiLink) return //m.reply('')
-    }
-    return !0
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*≡ Tautan terdeteksi*
+        Kami tidak mengizinkan link dari grup lain\n\n
+        Maaf, ${await conn.getName(m.sender)} Kamu Akan Di Kick Karena Melanggar Aturan`)
+    if (isAdmin) return m.reply('*Oh, Admin. Ya Maaf Bang, Namanya Manusia Bukan Nabi Boy*')
+    if (!isBotAdmin) return m.reply('*Bot Belum Admin_-*')
+    let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
+    let isLinkconnGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkconnGc.test(m.text)
+    if (isgclink) return m.reply(`*≡ Tautan terdeteksi*
+        Kami Tidak Akan Mengeluarkan Orang\n
+        Yang Mengirimkan Link Grup Ini`)
+    await conn.sendMessage(m.chat, { delete: m.key })
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+  }
+  return true
 }
+
+export default handler
