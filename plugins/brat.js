96900e81993b29edc26bd7bd941f1898b1906f62
hanipubaidur <hanifurohman05@gmail.com>
2024-12-10 20:47:33 +0700

    Fix, Gatau Dah Update Aja Makanya

--- plugins/brat.js
+++ plugins/brat.js
@@ -2,7 +2,7 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `Contoh penggunaan: ${usedPrefix + command} Crot Crot`;
-let stiker = await sticker(null, `https://ochinpo-helper.hf.space/brat?text=${text}`, '', '')
+let stiker = await sticker(null, `https://ochinpo-helper.hf.space/brat?text=${text}`, 'By', '𝘕𝘪𝘯𝘪𝘱𝘉𝘰𝘵 × 𝘌𝘭𝘢𝘪𝘯𝘢-𝘔𝘋')
 conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)  
 } 
 
