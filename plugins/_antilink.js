979a9385b384e7238198a909ebee62804484d6e0
hanipubaidur <hanifurohman05@gmail.com>
2024-12-10 07:32:41 +0700

    Fix Antilink

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,17 +7,13 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*≡ Tautan terdeteksi*
-        Kami tidak mengizinkan link dari grup lain\n\n
-        Maaf, ${await conn.getName(m.sender)} Kamu Akan Di Kick Karena Melanggar Aturan`)
+    await m.reply('*≡ Tautan terdeteksi*\nKami tidak mengizinkan link dari grup lain\n\nMaaf, Kamu Akan Di Kick Karena Melanggar Aturan')
     if (isAdmin) return m.reply('*Oh, Admin. Ya Maaf Bang, Namanya Manusia Bukan Nabi Boy*')
     if (!isBotAdmin) return m.reply('*Bot Belum Admin_-*')
     let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
     let isLinkconnGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkconnGc.test(m.text)
-    if (isgclink) return m.reply(`*≡ Tautan terdeteksi*
-        Kami Tidak Akan Mengeluarkan Orang\n
-        Yang Mengirimkan Link Grup Ini`)
+    if (isgclink) return m.reply('*≡ Tautan terdeteksi*\nKami Tidak Akan Mengeluarkan Orang\nYang Mengirimkan Link Grup Ini')
     await conn.sendMessage(m.chat, { delete: m.key })
     await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
   }
