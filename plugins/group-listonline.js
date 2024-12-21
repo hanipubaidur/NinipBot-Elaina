0135c83b23d1e250432856a5dbd28262e8c908ca
hanipubaidur <hanifurohman05@gmail.com>
2024-12-21 10:45:27 +0700

    Update Fitur

--- plugins/group-listonline.js
+++ plugins/group-listonline.js
@@ -1,26 +1,26 @@
 let handler = async (m, { conn }) => {
-    const c = await conn.groupMetadata(m.chat);
-    const online = Object.entries(conn.chats)
-      .filter(
-        ([k, v]) =>
-          k.endsWith("@s.whatsapp.net") &&
-          v.presences &&
-          c.participants.some((p) => k.startsWith(p.id)),
-      )
-      .sort((a, b) => a[0].localeCompare(b[0], "id", { sensitivity: "base" }))
-      .map(([k], i) => `*${i + 1}.* @${k.split("@")[0]}`)
-      .join("\n");
-  
-    let text = `*👤 LIST ONLINE*\n<==================>\n`;
-    text += online;
-  
-    conn.reply(m.chat, text, m);
-  };
-  
-  handler.help = ["listonline"].map((a) => a + " *[get list online member]*");
-  handler.tags = ["group"];
-  handler.command = ["listonline"];
-  handler.group = true;
-  handler.admin = true;
-  
-  export default handler;
+  const c = await conn.groupMetadata(m.chat);
+  const online = c.participants
+    .filter(p => p.id in conn.chats && conn.chats[p.id].presences)
+    .sort((a, b) => a.id.localeCompare(b.id, "id", { sensitivity: "base" }))
+    .map((p, i) => `*${i + 1}.* @${p.id.split("@")[0]}`)
+    .join("\n");
+
+  let text = `*👤 LIST ONLINE*\n<==================>\n`;
+  text += online;
+
+  conn.sendMessage(m.chat, {
+      text: text,
+      mentions: c.participants.map(p => p.id)
+  }, {
+      quoted: m
+  });
+};
+
+handler.help = ["listonline"].map((a) => a + " *[get list online member]*");
+handler.tags = ["group"];
+handler.command = ["listonline"];
+handler.group = true;
+handler.admin = true;
+
+export default handler;
